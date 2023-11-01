39361aa9aa882e114440fbafbca6134669639d43
= <tracy.sablon@gmail.com>
2023-11-01 17:21:32 +0100

    Initial Commit

---
+++ TestingCI/TestingCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingCIApp.swift
+//  TestingCI
+//
+//  Created by Tracy on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
