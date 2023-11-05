663801196a6f00d27baa9fbadbe6351173025827
= <tracy.sablon@gmail.com>
2023-11-05 17:41:16 +0100

    test 2
    resolve conflict and merge

--- TestingCI/ContentView.swift
+++ TestingCI/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Hello, world!")
+            Text("test 1")
+            Text("test 2")
         }
         .padding()
     }
